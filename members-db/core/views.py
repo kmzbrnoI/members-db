6f9dff3426729176e34b95824037a1870d27548c
celestian <petr.celestian@gmail.com>
2021-06-05 02:08:39 +0200

    OpenID

--- members-db/core/views.py
+++ members-db/core/views.py
@@ -27,14 +27,19 @@ class SiteHandler:
         return {'theme': self.theme, 'user': 'user', 'cities': cities, 'a_data': 'nothing'}
 
     async def auth_login(self, request):
-        uri = request.app['aiogoogle'].oauth2.authorization_url(
+        uri = request.app['aiogoogle'].openid_connect.authorization_url(
             include_granted_scopes=True,
             client_creds={
                 'client_id': request.app['cfg']['google-auth']['client_id'],
                 'client_secret':  request.app['cfg']['google-auth']['client_secret'],
-                'scopes': ['https://www.googleapis.com/auth/userinfo.profile'],
+                'scopes': [
+                    'https://www.googleapis.com/auth/userinfo.profile',
+                    'https://www.googleapis.com/auth/userinfo.email',
+                    'openid'
+                ],
                 'redirect_uri': 'http://celestian.cz:8080/auth/callback'
             },
+            nonce='haha_nonce'
         )
         return web.HTTPTemporaryRedirect(location=uri)
 
@@ -42,17 +47,29 @@ class SiteHandler:
     async def auth_callback(self, request):
         # Here we request the access and refresh token
         if request.query.get('code'):
-            full_user_creds = await request.app['aiogoogle'].oauth2.build_user_creds(
-                grant = request.query.get('code'),
+            full_user_creds = await request.app['aiogoogle'].openid_connect.build_user_creds(
+                grant=request.query.get('code'),
                 client_creds={
                     'client_id': request.app['cfg']['google-auth']['client_id'],
                     'client_secret':  request.app['cfg']['google-auth']['client_secret'],
-                    'scopes': ['https://www.googleapis.com/auth/userinfo.profile'],
+                    'scopes': [
+                        'https://www.googleapis.com/auth/userinfo.profile',
+                        'https://www.googleapis.com/auth/userinfo.email',
+                        'openid'
+                    ],
                     'redirect_uri': 'http://celestian.cz:8080/auth/callback'
-                }
+                },
+                nonce='haha_nonce',
+                verify=True
             )
             # Here, you should store full_user_creds in a db. Especially the refresh token and access token.
             logging.info(full_user_creds)
+
+            # A dict having claims of the user e.g. the sub claim and iat claim.
+            # Check https://developers.google.com/identity/protocols/oauth2/openid-connect#an-id-tokens-payload for more info
+            full_user_info = await request.app['aiogoogle'].openid_connect.get_user_info(full_user_creds)
+            logging.info(full_user_info)
+
             return {'theme': self.theme}
 
         else:
